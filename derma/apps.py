ef46d8e1f08bf549b2f46a830eb4bbab1a7e6215
pablo <pablocuri2015@hotmail.com>
2025-02-21 04:55:12 -0500

    recomendaction de recetar por modelo

--- derma/apps.py
+++ derma/apps.py
@@ -4,7 +4,8 @@ from django.apps import AppConfig
 from ultralytics import YOLO
 from dotenv import load_dotenv
 from pymongo import *
-
+import pandas as pd
+import joblib
 load_dotenv()
 
 
@@ -15,3 +16,5 @@ class DelliConfig(AppConfig):
     ingredient_classes = {0: 'ajo', 1: 'cebolla paiteña', 2: 'huevo', 3: 'leche', 4: 'limón',
                           5: 'papa', 6: 'plátano verde', 7: 'pollo', 8: 'queso', 9: 'tomate'}
     mongo_db = MongoClient(os.getenv('MONGO_HOST'))['delli']
+    recipes_df = pd.read_csv('models/recipes.csv')
+    recommendation_model = joblib.load('models/recomendation_model.pkl')
--- derma/service.py
+++ derma/service.py
@@ -5,6 +5,9 @@ from derma.serializers import RecipesSerializer
 from .apps import DelliConfig
 import supervision as sv
 import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from bson import ObjectId
 
 
 class RecipeService:
@@ -28,7 +31,7 @@ class RecipeService:
         _, buffer = cv2.imencode('.jpg', annotated_image)
         return buffer
 
-    def get_recipes_by_detected_ingredients(self, detected_ingredients):
+    def get_recipes_by_detected_ingredients_query(self, detected_ingredients):
         aggregate_pipeline = [{'$match':
                                    {'ingredients':
                                         {'$in': detected_ingredients}
@@ -47,7 +50,36 @@ class RecipeService:
         collection_recipes = DelliConfig.mongo_db['derma_recipes']
         return list(collection_recipes.aggregate(aggregate_pipeline))
 
+    def get_recipes_by_detected_ingredients_model(self, detected_ingredients):
+        encoding_detected_ingredients = self.one_hot_encoding(detected_ingredients)
+        ingredients_df = pd.DataFrame([encoding_detected_ingredients])
+        predicted_cluster = DelliConfig.recommendation_model.predict(ingredients_df)[0]
+        similar_recipes = self.get_recipes_by_cluster(predicted_cluster)
+        ingredients_df_vectors = ingredients_df.values
+        similar_recipes_vectors = similar_recipes.values
+        similarities = cosine_similarity(ingredients_df_vectors, similar_recipes_vectors)[0]
+        cluster_recipes = similar_recipes.copy()
+        cluster_recipes['similarity'] = similarities
+        cluster_recipes = cluster_recipes.sort_values(by='similarity', ascending=False)
+        collection_recipes = DelliConfig.mongo_db['derma_recipes']
+        recipes_id = list(map(lambda x: ObjectId(x), DelliConfig.recipes_df.loc[cluster_recipes.index].head()['_id'].values))
+        print(recipes_id)
+        return list(collection_recipes.find({'_id': {"$in":recipes_id}}))
+
+    def get_recipes_by_cluster(self, cluster):
+        return DelliConfig.recipes_df[DelliConfig.recipes_df['cluster'] == cluster][
+            DelliConfig.ingredient_classes.values()]
+
+    def one_hot_encoding(self, detected_ingredients):
+        one_hot = {ingredient: 0 for ingredient in DelliConfig.ingredient_classes.values()}
+        for ingredient in detected_ingredients:
+            if ingredient in one_hot:
+                one_hot[ingredient] = 1
+        return one_hot
+
     def get_precict_recipes(self, serializer):
         result = self.predict_ingredients(serializer)
         detected_ingredients = list(set(DelliConfig.ingredient_classes[int(cls)] for cls in result.boxes.cls))
-        return RecipesSerializer(self.get_recipes_by_detected_ingredients(detected_ingredients), many=True).data
+        if len(detected_ingredients) == 0:
+            return []
+        return RecipesSerializer(self.get_recipes_by_detected_ingredients_model(detected_ingredients), many=True).data
